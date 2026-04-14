bfe0f33884d95eeff1e9e2ffc1843a94309a581b
TanNguyen <nonemtrong0165@gmail.com>
2026-04-14 17:14:09 +0700

    test_pi

--- runs/detect/best_ncnn_model/model_ncnn.py
+++ runs/detect/best_ncnn_model/model_ncnn.py
@@ -4,12 +4,12 @@ import torch
 
 def test_inference():
     torch.manual_seed(0)
-    in0 = torch.rand(1, 3, 640, 640, dtype=torch.float)
+    in0 = torch.rand(1, 3, 480, 480, dtype=torch.float)
     out = []
 
     with ncnn.Net() as net:
-        net.load_param("runs/detect/yolov26_chinhthuc/weights/best_ncnn_model/model.ncnn.param")
-        net.load_model("runs/detect/yolov26_chinhthuc/weights/best_ncnn_model/model.ncnn.bin")
+        net.load_param("runs/detect/yolov26_vippro/weights/best_ncnn_model/model.ncnn.param")
+        net.load_model("runs/detect/yolov26_vippro/weights/best_ncnn_model/model.ncnn.bin")
 
         with net.create_extractor() as ex:
             ex.input("in0", ncnn.Mat(in0.squeeze(0).numpy()).clone())
